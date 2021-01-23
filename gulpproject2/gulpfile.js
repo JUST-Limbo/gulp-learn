0c537001c44f8e24186c18bd98ea45d356cae9ac
P Z <1034935762@qq.com>
2021-01-24 00:49:21 +0800

    2021年1月24日00:49:20

--- gulpproject2/gulpfile.js
+++ gulpproject2/gulpfile.js
@@ -16,7 +16,7 @@ const htmlmin = require("gulp-htmlmin");
 // })
 
 // gulp@4的语法
-// 需要在gulpfile.js文件里将这个函数名导出
+// 需要在gulpfile.js文件里通过module.exports将这个函数名导出
 // 命令行输入 gulp cssHandler 执行
 function cssHandler() {
   return gulp
@@ -61,9 +61,61 @@ function htmlHandler() {
     .pipe(gulp.dest('./dist/pages/'))
 }
 
+function imgHandler() {
+  return gulp
+    .src('./src/images/**')
+    .pipe(gulp.dest('./dist/images/'))
+}
+
+function videoHandler() {
+  return gulp
+    .src('./src/videos/**')
+    .pipe(gulp.dest('./dist/videos/'))
+}
+
+function audioHandler() {
+  return gulp
+    .src('./src/audios/**')
+    .pipe(gulp.dest('./dist/audios/'))
+}
+
+// 第三方文件
+function libHandler() {
+  return gulp
+    .src('./src/lib/**/*')
+    .pipe(gulp.dest('./dist/lib/'))
+}
+
+function fontsHandler() {
+  return gulp
+    .src('./src/fonts/**/*')
+    .pipe(gulp.dest('./dist/fonts/'))
+}
+
+/**
+ * 配置一个默认任务
+ * 目的是将所有任务一并执行
+ * gulp.series() gulp.parallel()
+ * 这两个api的返回值是一个函数,返回值可以直接被当做任务函数使用
+ * 使用task的方式创建一个default任务
+ * 方式1
+ * gulp.task('default',()=>{})
+ * 方式2
+ * module.exports.default=()=>{}
+ */
+
+// const res = gulp.parallel(cssHandler, jsHandler, sassHandler)
+// module.exports.default = res
+
 module.exports = {
   cssHandler,
   sassHandler,
   jsHandler,
-  htmlHandler
+  htmlHandler,
+  imgHandler,
+  videoHandler,
+  audioHandler,
+  libHandler,
+  fontsHandler,
 }
+module.exports.default = gulp.parallel(cssHandler, jsHandler, sassHandler, htmlHandler, imgHandler, libHandler, fontsHandler, videoHandler, audioHandler)
